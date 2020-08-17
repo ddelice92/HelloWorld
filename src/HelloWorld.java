daffb4d5fd98ea2ba6d5d4d3f934a88261c2ecef
doughnuttouch <67760292+doughnuttouch@users.noreply.github.com>
2020-08-17 08:20:14 -0400

    NOT SURE WHAT THIS IS

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,20 +1,24 @@
+//HERE IS SOME LINE AT THE TOP
+
 //*****************************
 //THIS IS A TEST LINE
 //*************
-
+//PROBLEM AREA ONE ECLIPSE
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		
+		//HERE IS SOME LINE IN THE MIDDLE
 
 	}
 
 }
-
+//PROBLEM AREA TWO ECLIPSE
 //****************
 //THIS CHANGE WAS MADE ON ECLIPSE INSTEAD OF GITHUB
 //*************
 //HERE IS ANOTHER GITHUB LINE
 //THIS IS WHAT IS SHOULD LOOK LIKE
 
-//ECLIPSE LINE AT END
+//HERE IS SOME LINE AT THE END
